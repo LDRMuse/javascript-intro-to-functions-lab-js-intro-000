bc600d2aed3a01046bba718ca32fad4afe899b83
Melissa Heying <melissakheying@gmail.com>
2020-05-03 20:10:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,16 @@ function logWhisper(string) {
   console.log('hello')
 }
 
-function logWhisper(string) {
-  string.toLowerCase('HELLO')
-  console.log()
+function logShout(string) {
+  string.toLowerCase()
+  console.log('HELLO')
 }
+/*describe('logShout(string)', function() {
+  it('takes a string argument and logs it in all caps using console.log()', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()*/
