bd44fd7b02734e1acf48b8092214892c6fa6234d
Melissa Heying <melissakheying@gmail.com>
2020-05-03 19:28:07 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+
+/*describe('shout(string)', function shout(string) {
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO')
+  })
+})*/
+
+function shout(string) {
+  return string.toUpperCase()
+}
